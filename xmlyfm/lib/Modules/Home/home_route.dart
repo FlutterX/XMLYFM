c7d6214e52a55849d59e10916c2656dc9bea94de
LinXunFeng <598600855@qq.com>
2018-12-22 17:36:47 +0800

    添加首页

---
+++ xmlyfm/lib/Modules/Home/home_route.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeRoute extends StatefulWidget {
+  @override
+  _HomeRouteState createState() => _HomeRouteState();
+}
+
+class _HomeRouteState extends State<HomeRoute> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("首页"),
+      ),
+    );
+  }
+}
