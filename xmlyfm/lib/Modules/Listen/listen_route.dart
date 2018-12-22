6d0c7ef5f4152cf43da718a8c73d4a931222a780
LinXunFeng <598600855@qq.com>
2018-12-22 17:37:00 +0800

    添加我听界面

---
+++ xmlyfm/lib/Modules/Listen/listen_route.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ListenRoute extends StatefulWidget {
+  @override
+    _ListenRouteState createState() => _ListenRouteState();
+}
+
+class _ListenRouteState extends State<ListenRoute> {
+  @override
+    Widget build(BuildContext context) {
+      return Text("我听");
+    }
+}
