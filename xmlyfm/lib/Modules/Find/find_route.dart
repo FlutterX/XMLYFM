41c694526a2b6137e2da5c257028ece42b4efe65
LinXunFeng <598600855@qq.com>
2018-12-22 17:36:31 +0800

    添加发现界面

---
+++ xmlyfm/lib/Modules/Find/find_route.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FindRoute extends StatefulWidget {
+  @override
+    _FindRouteState createState() => _FindRouteState();
+}
+
+class _FindRouteState extends State<FindRoute> {
+  @override
+    Widget build(BuildContext context) {
+      return Text("发现");
+    }
+}
