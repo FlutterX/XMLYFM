e2f612f77bd8bd862f5b24589ea0b24638c1bf1f
LinXunFeng <598600855@qq.com>
2018-12-24 22:14:23 +0800

    修改布局

--- xmlyfm/lib/Modules/Listen/listen_route.dart
+++ xmlyfm/lib/Modules/Listen/listen_route.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:xmlyfm/Configs/configs.dart';
+import 'package:xmlyfm/Modules/Listen/Header/listen_header.dart';
 
 class ListenRoute extends StatefulWidget {
   @override
@@ -6,8 +8,62 @@ class ListenRoute extends StatefulWidget {
 }
 
 class _ListenRouteState extends State<ListenRoute> {
+  double elevation = 0.0; // 0 | 0.6
+
   @override
     Widget build(BuildContext context) {
-      return Text("我听");
+      return Scaffold(
+        appBar: AppBar(
+          elevation: elevation,
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          title: Text('我听', style: TextStyle(color: Colors.black),),
+          actions: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(right: 15.0),
+              child: 
+                InkWell(
+                  onTap: () {
+                    print('搜索');
+                  },
+                  child: Icon(Icons.save_alt, size: 30.0, color: Colors.black,),
+                ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 15.0),
+              child: 
+                InkWell(
+                  onTap: () {
+                    print('搜索');
+                  },
+                  child: Icon(Icons.history, size: 30.0, color: Colors.black,),
+                ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 15.0),
+              child: 
+                InkWell(
+                  onTap: () {
+                    print('搜索');
+                  },
+                  child: Icon(Icons.search, size: 30.0, color: Colors.black,),
+                ),
+            ),
+          ],
+        ),
+        body: CustomScrollView(
+          slivers: <Widget>[
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Container(height: 80, child: ListenHeader(),),
+              ),
+            ),
+            // SliverToBoxAdapter(
+            //   child: TabBarView(),
+            // )
+          ],
+        ),
+      );
     }
 }
