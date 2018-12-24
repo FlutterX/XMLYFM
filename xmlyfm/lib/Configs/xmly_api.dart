c4962642728a393f3dec36cdf24b523493774ade
LinXunFeng <598600855@qq.com>
2018-12-24 22:11:34 +0800

    添加首页分类与详情API

---
+++ xmlyfm/lib/Configs/xmly_api.dart
@@ -0,0 +1,11 @@
+
+final httpScheme = "http://";
+final httpsScheme = "https://";
+
+// 网络
+final xmlyDomain = "mobwsa.ximalaya.com/";
+
+// 首页
+final home_category = httpScheme + xmlyDomain + "discovery-category/customCategories/1545621060064";
+// 首页详情
+final home_detail = httpScheme + xmlyDomain + "discovery-firstpage/v2/explore/ts-1545621064936";
