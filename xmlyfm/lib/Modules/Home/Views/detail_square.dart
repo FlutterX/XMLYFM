24b9f18c16f0c3d6023af14db0a0b5c66f834c82
LinXunFeng <598600855@qq.com>
2018-12-26 17:04:53 +0800

    Modify

--- xmlyfm/lib/Modules/Home/Views/detail_square.dart
+++ xmlyfm/lib/Modules/Home/Views/detail_square.dart
@@ -17,24 +17,24 @@ class DetailSquare extends StatelessWidget {
           crossAxisSpacing: 10.0,
           childAspectRatio: 1.0,
         ),
-        delegate: new SliverChildBuilderDelegate((context, index) {
+        delegate: SliverChildBuilderDelegate((context, index) {
           // 创建子widget
-          return new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.network(models[index].coverPath, width: 40.0),
-              Padding(
-                padding: EdgeInsets.only(top: 5.0),
-                child: Text(
-                  models[index].title,
-                  style: TextStyle(
-                      color: Colors.black54,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 13.0),
-                ),
-              )
-            ],
-          );
+          return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.network(models[index].coverPath, width: 40.0),
+                Padding(
+                  padding: EdgeInsets.only(top: 5.0),
+                  child: Text(
+                    models[index].title,
+                    style: TextStyle(
+                        color: Colors.black54,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 13.0),
+                  ),
+                )
+              ],
+            );
         }, childCount: models.length),
       ),
     );
