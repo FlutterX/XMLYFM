908a92a7df0440e494e3cc472a0fae26fcce4260
LinXunFeng <598600855@qq.com>
2018-12-26 17:02:55 +0800

    添加首页推荐api

--- xmlyfm/lib/Configs/xmly_api.dart
+++ xmlyfm/lib/Configs/xmly_api.dart
@@ -9,3 +9,5 @@ final xmlyDomain = "mobwsa.ximalaya.com/";
 final home_category = httpScheme + xmlyDomain + "discovery-category/customCategories/1545621060064";
 // 首页详情
 final home_detail = httpScheme + xmlyDomain + "discovery-firstpage/v2/explore/ts-1545621064936";
+// 首页推荐
+final home_recommend = httpScheme + xmlyDomain + "subscribe/v2/subscribe/home/recommend";
